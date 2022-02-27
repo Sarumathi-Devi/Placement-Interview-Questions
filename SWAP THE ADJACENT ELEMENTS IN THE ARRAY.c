929cf3cb86a6b230dffb989631e9d93d5263104a
arulmozhi-ec19 <100395639+arulmozhi-ec19@users.noreply.github.com>
2022-02-27 23:35:56 +0530

    Create SWAP THE ADJACENT ELEMENTS IN THE ARRAY.c

---
+++ SWAP THE ADJACENT ELEMENTS IN THE ARRAY.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for ( int i = 0; i < n; i++ ) scanf("%d",&a[i]);
+    int temp = 0;
+    for ( int i = 0; i < n-1; i += 2 )
+    {
+        temp = a[i];
+        a[i] = a[i+1];
+        a[i+1] = temp;
+    }
+    for ( int i = 0; i < n; i++ ) printf("%d ",a[i]);
+    return 0;
+}
