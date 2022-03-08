8a8e0f359d23be96b095877123ffec7923485ed8
arulmozhi-ec19 <100395639+arulmozhi-ec19@users.noreply.github.com>
2022-03-08 22:00:58 +0530

    Create Mirror Image Patter Printing.cpp

---
+++ Mirror Image Patter Printing.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cin >> num;
+    int i,k;
+    int j = num-1;
+    int var1 = 1;
+    int var2 = 1;
+    
+    i = 0;
+    while ( i < num-1 )
+    {
+        j = num-1-i;
+        while ( j > 0 )
+        {
+            cout << " ";
+            j--;
+        }
+        k = 1;
+        while ( k <= var1 )
+        {
+            cout << abs(k-var2);
+            k++;
+        }
+        cout << endl;
+        var1 += 2;
+        var2 += 1;
+        i++;
+    }
+    return 0;
+}
