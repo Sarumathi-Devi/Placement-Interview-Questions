9b3094aca44a8ba7d94de8022f6f8cf9b999c69b
arulmozhi-ec19 <100395639+arulmozhi-ec19@users.noreply.github.com>
2022-03-20 18:42:44 +0530

    Create Generate prime numbers till range n.cpp

---
+++ Generate prime numbers till range n.cpp
@@ -0,0 +1,39 @@
+// This algorithm is named as SIEVE OF ERATOSTHENES
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number = ";
+    cin >> n;
+    int ctr = 0;
+    bool prime[n+1];
+    memset ( prime, true, sizeof(prime) );
+    cout << endl;
+    int count = 0;
+    int start;
+    
+    start = 2;
+    while ( start < sqrt(n) )
+    {
+        if ( prime[start] == true )
+        {
+            ctr = start*start;
+            while ( ctr <= n )
+            {
+                prime[ctr] = false;
+                ctr += start;
+            }
+        }
+        start += 1;
+    }
+    cout << "The prime numbers till " << n << " are ";
+    ctr = 2;
+    while ( ctr < n )
+    {
+        if ( prime[ctr] == true ) cout << ctr << " ";
+        ctr++;
+    }
+    cout << count;
+}
