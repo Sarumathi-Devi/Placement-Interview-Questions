de5efa87e6fe35b65c16bc4528a398c53c4dd3ba
ARULMOZHI K <100395639+arulmozhi-ec19@users.noreply.github.com>
2022-04-21 18:26:13 +0530

    Update Pangram.cpp

--- Pangram.cpp
+++ Pangram.cpp
@@ -1,36 +1,39 @@
 //  A pangram is a sentence containing every letter in the English Alphabet.
 // INPUT : "The quick brown fox jumps over the lazy dog"
 // OUTPUT : YES
-
 #include <bits/stdc++.h>
-#include <string.h>
-#include <algorithm> // for transform function
 using namespace std;
-
-bool isAnagram ( string str )
+int main()
 {
-    transform ( str.begin(), str.end(), str.begin(), :: tolower ); // stl to convert all uppercase to lowercase
-    if ( str.size() < 26 ) return false;
-    
-    int hash[26] = {0};
+    string str = "the quick brown fox jumps over a lazy dog";
+    int hash[str.length()] = {0};
     
     int idx = 0;
-    while ( idx < str.size() )  
-        hash[str[idx++] - 'a'] = 1;  // frequency checking 
+    while ( idx < str.length() )
+    {
+        if ( str[idx] == ' ' ) idx += 1;
+        if ( hash[str[idx]-'a'] == 0 ) hash[str[idx] - 'a'] += 1;
+        idx += 1;
+    }
+    
+    /*idx = 0;
+    while ( idx < 26 )
+    {
+        cout << char(idx + 'a') << "----" << hash[idx] << endl;
+        idx += 1;
+    }*/ // code for checking frequency 
     
     idx = 0;
     while ( idx < 26 )
-        if ( hash[idx++] != 1) 
-            return false;      
-    return true;
-}
-
-int main()
-{
-    string str;
-    getline(cin,str);
+    {
+        if ( hash[idx++] != 1 ) 
+        {
+            cout << "Not a Pangram";
+            break;
+        }
+    }
     
-    cout << (isAnagram(str)?"Yes":"No");
+    if ( idx == 26 ) cout << "Pangram";
     
     return 0;
 }
